8e0483c10004a7a3d9fd5b073a41f55f3618fee7
ianannoh <118678855+ianannoh@users.noreply.github.com>
2025-07-14 19:06:52 +0000

    Router
    Added home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {Appname} from '../misc/Appname';
+import {AppComponent} from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: AppComponent,
+    title: `${Appname.appName} - Home`,
+  }
+];
---
+++ src/misc/Appname.ts
@@ -0,0 +1,5 @@
+export class Appname {
+
+  static appName: string = "VDT";
+
+}
