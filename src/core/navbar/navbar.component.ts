a4eb8c2fa68946376bdfe9719f02572633358fc1
ianannoh <118678855+ianannoh@users.noreply.github.com>
2025-07-17 17:41:38 +0000

    pages/home
    added styles to navbar

--- src/core/navbar/navbar.component.ts
+++ src/core/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {NgIf} from '@angular/common';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import {NgIf,  isPlatformBrowser} from '@angular/common';
 import  AOS from 'aos';
 
 @Component({
@@ -11,8 +11,13 @@ import  AOS from 'aos';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit {
-  ngOnInit(): void {
-      AOS.init({})
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
   }
 
   languages: any[] = [
