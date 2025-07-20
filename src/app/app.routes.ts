f49ba851cc98b4c28900df4e0a49e3bb949ece15
ianannoh <ianlighter23@gmail.com>
2025-07-20 20:17:25 +0000

    feat/nav bar
    added projects route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import {Appname} from '../misc/Appname';
 import {HomeComponent} from '../pages/home/home.component';
+import {ProjectsComponent} from '../pages/projects/projects.component';
 
 export const routes: Routes = [
 
   {
     path: '',
     component: HomeComponent,
-    title: `${Appname.appName}`,
+    title: `${Appname.appName}`
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+    title: `${Appname.appName} - Projects`,
   }
 ];
